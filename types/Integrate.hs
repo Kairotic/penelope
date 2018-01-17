922e04a77a01cd5a8653fb30c9e59e763348cdd5
alex <alex@slab.org>
2018-01-17 10:19:49 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -27,6 +27,7 @@ data Thread = Strand (Colour Double) Spin
 
 data Structure = Structure Thread Action
 
+-- Probably not right to conflate S/Z threading with S/Z turning
 data Tablet = Tablet {warps :: [Thread], flip :: Twist}
 
 data TabletLoom = TabletLoom {tablet :: Tablet, weft :: Thread}
