b03f1da219eb055eab76d5faf0ce28a793e0e5be
alex <alex@slab.org>
2018-01-17 09:06:33 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -5,6 +5,13 @@ import Data.Colour.Names
 
 data Movement = Pull Int | TurnIn | TurnOut | Over | Under
 
+instance Show Movement where
+  show (Pull n) = "pull " ++ show n
+  show TurnIn = "turn in"
+  show TurnOut = "turn out"
+  show Over = "over"
+  show Under = "under"
+
 data Thread = Strand (Colour Double) | Ply [Thread]
 
 data Structure = Structure Thread Movement
