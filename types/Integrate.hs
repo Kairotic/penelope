bd51e1735dc131bbcc2f5f60def359869d464903
alex <alex@slab.org>
2018-01-17 09:03:59 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -1,5 +1,8 @@
 module Integrate where
 
+import Data.Colour.SRGB
+import Data.Colour.Names
+
 data Movement = Pull Int | TurnIn | TurnOut | Over | Under
 
 data Thread = Strand ColourD | Ply [Thread]
