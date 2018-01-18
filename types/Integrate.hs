de1a611359806763534086d5b4460b5dffa2682e
alex <alex@slab.org>
2018-01-18 15:09:16 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -5,7 +5,10 @@ import Data.Colour.Names
 
 data Action = Pull Int | Turn | TurnBack | Over | Under
 data Twist = S | Z
+           deriving Show
+                     
 data Spin = Spin [Twist]
+          deriving Show
 
 instance Show Action where
   show (Pull n) = "pull " ++ show n
