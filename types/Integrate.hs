97956cb771ccb7de64390a022dd4208caf4bb619
alex <alex@slab.org>
2018-01-18 15:55:31 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -10,7 +10,7 @@ data Twist = S | Z
 data Spin = Spin [Twist]
 
 instance Show Spin where
-  show (Spin s) = (map show $ take 12 s) ++ ", .."
+  show (Spin s) = (map (\x -> [show x]) $ take 12 s) ++ ", .."
 
 instance Show Action where
   show (Pull n) = "pull " ++ show n
