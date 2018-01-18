09b4ef1a673ae692cc65869a0656756ea709b8e4
alex <alex@slab.org>
2018-01-18 16:03:10 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -35,11 +35,12 @@ data Thread = Strand {colour :: Colour Double, roll :: Spin}
 
 instance Show Thread where
   show (Strand c r) = "Strand " ++ showColour c ++ " " ++ show r
-    where showColour r | r == red = "red"
-                       | r == blue = "blue"
-                       | r == green = "green"
-                       | r == white = "white"
-                       | otherwise = show r
+    where showColour c | c == red = "red"
+                       | c == blue = "blue"
+                       | c == green = "green"
+                       | c == white = "white"
+                       | otherwise = show c
+  show (Ply pt r) = "Ply " ++ show pt ++ show r
 
 -- Trying to conflate S/Z threading (or flip) on the card with
 -- rotation of it by referring to former as 'yaw' and latter as
