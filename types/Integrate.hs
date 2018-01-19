3d12d16d55a34aa4819826461aed5fa32f8011cb
alex <alex@slab.org>
2018-01-19 13:13:57 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -85,7 +85,7 @@ data TabletLoom = TabletLoom {tablets :: [Tablet], tabletWeft :: Thread}
 
 -- Not the shed exactly but how to go from one shed to the
 -- next.. Should be one twist per card. This is really the same type
--- as a Spin although used differently. They should probably be unified.
+-- as a Spin although used differently, hmm.
 type TabletShed = [Twist]
 
 data TabletWeave = TabletWeave {tLoom :: TabletLoom, tSheds :: [TabletShed]}
