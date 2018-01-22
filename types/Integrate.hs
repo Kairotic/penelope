1ebd85e48d3827a70d83fdda67461247b571f2c6
alex <alex@slab.org>
2018-01-22 13:51:40 +0000

    - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -179,7 +179,7 @@ test = TabletWeave {tLoom = TabletLoom {tablets = take 12 $ cycle [redWhiteTable
 -- The resulting band
 testBand = tabletWeave test
 
-simpleWeave = TabletWeave {tLoom = TabletLoom {tablets = take 12 $ cycle [rwgbTablet,rwgbTablet'
+simpleWeave = TabletWeave {tLoom = TabletLoom {tablets = take 12 $ cycle [rwgbTablet,flipTablet rwgbTablet
                                                                          ],
                                                tabletWeft = Strand {colour = orange,
                                                                     roll = Spin (repeat S)
