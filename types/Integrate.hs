a418548b5d8dc3164926c045255111d8a00bfe24
alex <alex@slab.org>
2018-01-22 11:55:25 +0000

    / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -84,7 +84,7 @@ plyTop n (Ply pt (Spin (twist:twists)))
           (!!!) xs n = xs !! (n `mod` length xs)
 
 showSegments ss = concat $ map (\(f, s) -> f s)
-                  $ zip (repeat [colourTwistEven, colourTwistOdd]) (concat $ transpose ss) 
+                  $ zip (repeat [colourTwistEven, colourTwistOdd]) (concat $ transpose ss)
 
 showBand b = showSegments $ map (plyTop 0) $ bandCords b
 
