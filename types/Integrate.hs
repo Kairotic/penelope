85c16142c1b73107153d4826e39cc03d1e138a73
alex <alex@slab.org>
2018-01-28 20:35:21 +0000

    - ‘types/#Integrate.hs#’ - ‘types/.#Integrate.hs’ / ‘types/Integrate.hs’

--- types/Integrate.hs
+++ types/Integrate.hs
@@ -222,7 +222,12 @@ svgWeft d t x y ((Pull n):as) = svg ++ svgWeft d x' y' as
                 | d == Down  = (x, y+n)
                 | d == Left  = (x-n, y)
                 | d == Right = (x+n, y)
-        svg = "<path d=\"m " ++ path cnr x y ++ " z\" style=\"fill:" ++ cHex red ++ ";fill-rule:evenodd;stroke:none\" />"
+        svg = "<path d=\"m " ++ path ++ " z\" style=\"fill:" ++ cHex red ++ ";fill-rule:evenodd;stroke:none\" />"
+        path = show (x*svgScale) ++ "," ++ show (y*svgScale) ++ " "
+               ++ (show x  )
+
+-- path TL x y = show x ++ "," ++ show y ++ " 10,0 -10,10"
+
 
 svgPreamble = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<svg\n   xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n   xmlns:cc=\"http://creativecommons.org/ns#\"\n   xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n   xmlns:svg=\"http://www.w3.org/2000/svg\"\n   xmlns=\"http://www.w3.org/2000/svg\"\n   version=\"1.1\"\n   id=\"svg2\"\n   viewBox=\"0 0 744.09448819 1052.3622047\"\n   height=\"297mm\"\n   width=\"210mm\">\n  <defs\n     id=\"defs4\" />\n  <metadata\n     id=\"metadata7\">\n    <rdf:RDF>\n      <cc:Work\n         rdf:about=\"\">\n        <dc:format>image/svg+xml</dc:format>\n        <dc:type\n           rdf:resource=\"http://purl.org/dc/dcmitype/StillImage\" />\n        <dc:title></dc:title>\n      </cc:Work>\n    </rdf:RDF>\n  </metadata>\n  <g\n     id=\"layer1\">\n"
 
