58d17f866e166efd576c4852bce1b056bd889657
alex <alex@slab.org>
2018-04-15 23:59:28 +0200

    - ‘types/#Scad.hs#’ - ‘types/.#Scad.hs’ / ‘types/Scad.hs’

--- types/Scad.hs
+++ types/Scad.hs
@@ -7,4 +7,8 @@ import Tablet
 scadBand :: Band -> String
 scadBand (Band cords weft) = concatMap showThread cords
 
-showThread t = ""
+showThread (Ply ts s)  = ""
+  where colours = map firstColour ts
+        setColour name c = name ++ " = [" ++ show r ++ "," ++ show g ++ "," ++ show b ++ "];\n"
+          where (RGB r g b) = toSRGB c
+showThread _ = ""
