c5d1d90118569301e9c3cdc51c56e1b194123ca5
alex <alex@slab.org>
2018-04-16 00:16:25 +0200

    - ‘types/.#Scad.hs’ < ‘types/#Scad.hs#’ > ‘types/Scad.hs’

--- types/Scad.hs
+++ types/Scad.hs
@@ -7,8 +7,12 @@ import Tablet
 scadBand :: Band -> String
 scadBand (Band cords weft) = concatMap showThread cords
 
-showThread (Ply ts s)  = concatMap setColour $ zip (words "a b c d") colours
-  where colours = map firstColour ts
+showThread (Ply threads twists)  = setColours ++ doTwist 0 twists
+  where doTwist d (S:twists) = doTwist (d-90) twists
+        doTwist d (Z:twists) = doTwist (d+90) twists
+        doTwist d (I:twists) = doTwist (d+0 ) twists
+        setColours = concatMap setColour $ zip (words "a b c d") colours
+        colours = map firstColour ts
         setColour (name, c) = name ++ " = [" ++ show r ++ "," ++ show g ++ "," ++ show b ++ "];\n"
           where (RGB r g b) = toSRGB c
 showThread _ = ""
