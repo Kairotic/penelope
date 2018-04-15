5caeb2dcb3b6e0609c1e52bcdaa42d42cc9b2690
alex <alex@slab.org>
2018-04-16 00:30:40 +0200

    / ‘types/Scad.hs’

--- types/Scad.hs
+++ types/Scad.hs
@@ -7,13 +7,14 @@ import Tablet
 scadBand :: Band -> String
 scadBand (Band cords weft) = concatMap showThread cords
 
-showThread (Ply threads twists)  = setColours ++ doTwist 0 0 twists
-  where doTwist d y (S:twists) = seg (-90) (d - 90) y ++ doTwist (d - 90) (y+1) twists
-        doTwist d y (Z:twists) = seg ( 90) d (d + 90) y ++ doTwist (d + 90) (y+1) twists
+showThread (Ply threads (Spin twists))  = setColours ++ doTwist 0 0 twists
+  where doTwist :: Int -> Int -> [Twist] -> String
+        doTwist d y (S:twists) = seg (-90) (d - 90) y ++ doTwist (d - 90) (y+1) twists
+        doTwist d y (Z:twists) = seg ( 90) (d + 90) y ++ doTwist (d + 90) (y+1) twists
         doTwist d y (I:twists) = seg (  0) (d +  0) y ++ doTwist (d +  0) (y+1) twists
         seg t r y = "rotate([0,0," ++ show r ++ "]) translate([0,0," ++ y*2 "]) yarnSegment(" ++ show t ++ ",a,b,c,d);"
         setColours = concatMap setColour $ zip (words "a b c d") colours
-        colours = map firstColour ts
+        colours = map firstColour threads
         setColour (name, c) = name ++ " = [" ++ show r ++ "," ++ show g ++ "," ++ show b ++ "];\n"
           where (RGB r g b) = toSRGB c
 showThread _ = ""
