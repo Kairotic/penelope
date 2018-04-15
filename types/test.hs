f933734d052583d04254e46d79ac84a9980e928b
alex <alex@slab.org>
2018-04-15 21:28:14 +0200

    - ‘types/#test.hs#’ - ‘types/.#test.hs’ / ‘types/test.hs’

--- types/test.hs
+++ types/test.hs
@@ -46,3 +46,26 @@ fooWeave = TabletWeave {tLoom = TabletLoom {tablets = take 12 $ cycle [rwgbTable
   where rwgbTablet = Tablet {warps = [thread red, thread white, thread green, thread blue], yaw = S}
         
 fooBand = tabletWeave fooWeave
+
+
+
+daveWeave :: TabletWeave
+daveWeave = TabletWeave {tLoom = TabletLoom {tablets = [Tablet [a,a,a,a] S,
+                                                        Tablet [y,z,x,y] S,
+                                                        Tablet [y,y,z,x] S,
+                                                        Tablet [x,y,y,z] S,
+                                                        Tablet [x,y,y,z] Z,
+                                                        Tablet [y,y,z,x] Z,
+                                                        Tablet [y,z,x,y] Z,
+                                                        Tablet [a,a,a,a] Z
+                                                       ],
+                                             tabletWeft = Strand {colour = orange,
+                                                                  roll = Spin (repeat S)
+                                                                 }
+                                            },
+                       tSheds = [S,S,S,S,Z,Z,Z,Z]
+                      }
+  where a = thread yellow
+        x = thread brown
+        y = thread white
+        z = thread red
