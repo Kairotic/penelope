bb34ae4a196930873cf2c9db30ae0db9520ea249
alex <alex@slab.org>
2018-04-15 14:29:38 +0100

    / ‘types/test.hs’ / ‘types/test.pdf’ / ‘types/test.svg’ / ‘types/todo.txt’

--- types/test.hs
+++ types/test.hs
@@ -17,3 +17,19 @@ simpleWeave = TabletWeave {tLoom = TabletLoom {tablets = take 12 $ cycle [rwgbTa
   where rwgbTablet = Tablet {warps = [thread red, thread white, thread green, thread blue], yaw = S}
 
 simpleBand = tabletWeave simpleWeave
+
+
+
+bfWeave :: TabletWeave
+bfWeave = TabletWeave {tLoom = TabletLoom {tablets = take 12 $ cycle [rwgbTablet,
+                                                                      flipTablet rwgbTablet
+                                                                     ],
+                                           tabletWeft = Strand {colour = orange,
+                                                                roll = Spin (repeat S)
+                                                               }
+                                          },
+                       tSheds = (take 24 $ cycle [replicate 12 S, replicate 12 S, replicate 12 Z, replicate 12 Z])
+                      }
+  where rwgbTablet = Tablet {warps = [thread red, thread white, thread green, thread blue], yaw = S}
+        
+bfBand = tabletWeave bfWeave
