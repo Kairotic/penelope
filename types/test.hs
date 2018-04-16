c5d94f5c8dfb0f906b67ac56a6ff647415cea21a
alex <alex@slab.org>
2018-04-16 10:03:21 +0200

    - ‘types/.#test.hs’ / ‘types/test.hs’

--- types/test.hs
+++ types/test.hs
@@ -56,17 +56,13 @@ daveWeave =
                                                Tablet [y,z,x,y] Z,
                                                Tablet [y,y,z,x] Z,
                                                Tablet [x,y,y,z] Z,
-                                               Tablet [x,y,y,z] S,                                                                                     Tablet [y,y,z,x] S,
-                                               Tablet [y,z,x,y] S,
-                                               Tablet [a,a,a,a] S
                                                -- TODO - threading
                                                -- should be done
                                                -- before the flip (?)
-                                               {-
                                                Tablet [x,z,y,y] S,
                                                Tablet [y,x,z,y] S,
                                                Tablet [y,y,x,z] S,
-                                               Tablet [a,a,a,a] S-}
+                                               Tablet [a,a,a,a] S
                                              ],
                                     tabletWeft = thread orange
                                   },
