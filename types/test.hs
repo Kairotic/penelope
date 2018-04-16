892951028fc2ec52adefda8ce37e6c5972b73408
alex <alex@slab.org>
2018-04-16 10:12:55 +0200

    - ‘types/.#test.hs’ / ‘types/test.hs’

--- types/test.hs
+++ types/test.hs
@@ -53,7 +53,7 @@ fooBand = tabletWeave fooWeave
 daveWeave :: TabletWeave
 daveWeave =
   TabletWeave {tLoom = TabletLoom {tablets = [Tablet [y,y,y,y] Z,
-                                               Tablet [r,w,r,r] Z,
+                                               Tablet [w,r,b,w] Z,
                                                Tablet [w,w,r,b] Z,
                                                Tablet [b,w,w,r] Z,
                                                Tablet [b,w,w,r] S,
