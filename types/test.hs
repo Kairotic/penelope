ec2e15ec49f8b9ab7995f2a783d8692ad0e976b7
alex <alex@slab.org>
2018-04-16 12:38:00 +0200

    - ‘types/#test.hs#’ - ‘types/.#test.hs’ / ‘types/test.hs’

--- types/test.hs
+++ types/test.hs
@@ -52,26 +52,14 @@ fooBand = tabletWeave fooWeave
 
 daveWeave :: TabletWeave
 daveWeave =
-  TabletWeave {tLoom = TabletLoom {tablets = [Tablet [y,y,y,y] Z,
-                                               Tablet [w,r,b,w] Z,
-                                               Tablet [w,w,r,b] Z,
-                                               Tablet [b,w,w,r] Z,{-
-                                               Tablet [b,w,w,r] S,
-                                               Tablet [w,w,r,b] S,
-                                               Tablet [w,r,b,w] S,
-                                               Tablet [y,y,y,y] S-}
-        
-                                               -- TODO - threading
-                                               -- should be done
-                                               -- before the flip (?)
-                                               Tablet [b,r,w,w] S,
-                                               Tablet [w,b,r,w] S,
-                                               Tablet [w,w,b,r] S,
-                                               Tablet [y,y,y,y] S
-                                             ],
-                                    tabletWeft = thread orange
-                                  },
-                tSheds = map (replicate 8) [S,S,S,S,Z,Z,Z,Z]
+  TabletWeave {tLoom =
+    TabletLoom {tablets =
+                [Tablet [y,y,y,y] Z, Tablet [w,r,b,w] Z, Tablet [w,w,r,b] Z, Tablet [b,w,w,r] Z,
+                  Tablet [b,r,w,w] S, Tablet [w,b,r,w] S, Tablet [w,w,b,r] S, Tablet [y,y,y,y] S
+                ],
+                tabletWeft = thread orange
+               },
+               tSheds = map (replicate 8) [S,S,S,S,Z,Z,Z,Z]
               }
   where y = thread yellow
         b = thread brown
