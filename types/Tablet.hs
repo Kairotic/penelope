9a89270d2dbd861f03220bb94c40c765dfe3102b
alex <alex@slab.org>
2018-04-16 21:27:25 +0200

    / ‘types/Tablet.hs’

--- types/Tablet.hs
+++ types/Tablet.hs
@@ -8,7 +8,7 @@ import System.Process
 -- References:
 -- http://www.stringpage.com/tw/twtheory.html
 -- http://www.pawfal.org/dave/blog/tag/tablet-weaving/
--- 
+-- http://dyadic.borrowed-scenery.net/penelope/tabletloom/
 
 -- 'Pull' is a thread over a given distance (generally under tension)
 -- 'Turn' is where a thread takes the same 90 degree turn as last time
