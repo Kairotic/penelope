a1e42fbe4c1b36691cc36196c46028edc159d75e
alex <alex@slab.org>
2018-04-16 09:57:48 +0200

    / ‘types/Scad.hs’ / ‘types/test.scad’

--- types/Scad.hs
+++ types/Scad.hs
@@ -6,7 +6,7 @@ import Tablet
 
 scadBand :: Band -> String
 scadBand (Band cords weft) = scadPreamble ++ (concat $ map offset $ zip [0, 2 ..] (map showThread cords))
-  where offset (n, t) = "translate([0," ++ show n ++ ",0]) {\n" ++ t ++ "};\n"
+  where offset (n, t) = "translate([0," ++ show (n*3) ++ ",0]) {\n" ++ t ++ "};\n"
 
 writeScadBand b = writeFile "test.scad" (scadBand b)
 
@@ -16,7 +16,7 @@ showThread (Ply threads (Spin twists))  = setColours ++ "rotate ([0,0,45]) {\n" 
         doTwist d y (Z:twists) = seg ( 90) (d + 90) y ++ doTwist (d + 90) (y-1) twists
         doTwist d y (S:twists) = seg (-90) (d - 90) y ++ doTwist (d - 90) (y-1) twists
         doTwist d y (I:twists) = seg (  0) (d +  0) y ++ doTwist (d +  0) (y-1) twists
-        seg t r y = "rotate([0,0," ++ show r ++ "]) translate([0,0," ++ show (y*4) ++ "]) yarnSegment(" ++ show t ++ ",a,b,c,d);\n"
+        seg t r y = "rotate([0,0," ++ show r ++ "]) translate([0,0," ++ show (y*2) ++ "]) yarnSegment(" ++ show t ++ ",a,b,c,d);\n"
         setColours = concatMap setColour $ zip (words "a b c d") colours
         colours = map firstColour threads
         setColour (name, c) = name ++ " = [" ++ show r ++ "," ++ show g ++ "," ++ show b ++ "];\n"
