8de66a96e523cf5d7f68067cca6b80ae71c3b318
alex <alex@slab.org>
2018-04-18 16:42:03 +0200

    / ‘types/test.hs’

--- types/test.hs
+++ types/test.hs
@@ -234,7 +234,7 @@ heartWeave =
                                       threadTablet [b,b,w,w] Z,
                                       threadTablet [b,b,w,w] Z,
                                       threadTablet [b,b,w,w] Z,
-                                      threadTablet [b,b,w,w] Z,
+                                      threadTablet [b,b,w,w] Z
                           ],
                 tabletWeft = thread orange
                },
