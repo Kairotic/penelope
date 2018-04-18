2a9985192bb3e1dde5f922aed3f544765c1ca483
alex <alex@slab.org>
2018-04-18 16:44:39 +0200

    - ‘types/.#test.hs’ / ‘types/test.hs’ / ‘types/test.scad’

--- types/test.hs
+++ types/test.hs
@@ -228,12 +228,12 @@ heartWeave =
   TabletWeave {tLoom =
     TabletLoom {tablets = [
                                       threadTablet [w,w,b,b] S,
-                                      threadTablet [w,b,b,w] S,
+                                      threadTablet [b,b,w,w] S,
                                       threadTablet [b,b,w,w] S,
                                       threadTablet [w,b,b,w] S,
                                       threadTablet [w,b,b,w] Z,
                                       threadTablet [b,b,w,w] Z,
-                                      threadTablet [w,b,b,w] Z,
+                                      threadTablet [b,b,w,w] Z,
                                       threadTablet [w,w,b,b] Z
                           ],
                 tabletWeft = thread orange
