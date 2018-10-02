8e6ed1edf157de0593f5a46761ca22794e019302
alex <alex@slab.org>
2018-10-02 16:26:03 +0100

    - ‘sense/.#trigger.py’ / ‘sense/trigger.py’

--- sense/trigger.py
+++ sense/trigger.py
@@ -3,7 +3,7 @@ import serial
 
 devname = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_557363239393515181E2-if00"
 
-with serial.Serial('/dev/ttyS1', 19200, timeout=1) as ser:
+with serial.Serial('/dev/ttyS1', 115200, timeout=1) as ser:
     s = serial.Serial(devname)
     print("opened serial port " + ser.name)
 
