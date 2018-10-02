8ef51257c7cf4f944e0a5d9ab7c89de9f258269e
alex <alex@slab.org>
2018-10-02 17:13:10 +0100

    - ‘sense/.#trigger.py’ / ‘sense/trigger.py’

--- sense/trigger.py
+++ sense/trigger.py
@@ -2,31 +2,11 @@
 
 import serial
 import re
+import liblo
 
+target = liblo.Address(6010)
 
-
-devname = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_557363239393515181E2-if00"
-
-pat = re.compile("(\+|\-)(\d+)\s*([\d\.]+)?\s*([\d\.]+)?")
-
-with serial.Serial(devname, 115200, timeout=1) as ser:
-    print("opened serial port " + ser.name)
-
-    while True:
-        l = ser.readline()
-        if l:
-            print l
-            m = re.search(pat, l)
-            if m:
-                if m.group(1) == "+":
-                    n = int(m.group(2))
-                    filtered = int(m.group(3))
-                    baseline = int(m.group(4))
-                    
-                print("%s %d %f %f" % (m.group(1), int(m.group(2)), int(m.group(3)), int(m.group(4))))
-
-            
-    target = liblo.Address(6010)
+def dirt():
     liblo.send(target,
                "/play",
                "bd", 
@@ -62,3 +42,27 @@ with serial.Serial(devname, 115200, timeout=1) as ser:
                0, # hold
                0  # release
                )
+
+
+devname = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_557363239393515181E2-if00"
+
+pat = re.compile("(\+|\-)(\d+)\s*([\d\.]+)?\s*([\d\.]+)?")
+
+with serial.Serial(devname, 115200, timeout=1) as ser:
+    print("opened serial port " + ser.name)
+
+    while True:
+        l = ser.readline()
+        if l:
+            print l
+            m = re.search(pat, l)
+            if m:
+                if m.group(1) == "+":
+                    n = int(m.group(2))
+                    filtered = int(m.group(3))
+                    baseline = int(m.group(4))
+                    
+                print("%s %d %f %f" % (m.group(1), int(m.group(2)), int(m.group(3)), int(m.group(4))))
+                dirt()
+
+            
