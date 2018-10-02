52b26a21eade949ad62ec2d79011fda8ad624b51
alex <alex@slab.org>
2018-10-02 17:46:22 +0100

    / ‘sense/trigger.py’

--- sense/trigger.py
+++ sense/trigger.py
@@ -10,7 +10,7 @@ def dirt():
     print "kick"
     liblo.send(target,
                "/play",
-               0,0,
+               1,0,
                1.0, # cps
                "gabba", 
                float(0.0), # offset
