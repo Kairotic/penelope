98032154678349f9341e83784d34406ab4299bfd
alex <alex@slab.org>
2018-10-02 16:32:20 +0100

    - ‘sense/#trigger.py#’ - ‘sense/.#trigger.py’ / ‘sense/trigger.py’

--- sense/trigger.py
+++ sense/trigger.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
+
 import serial
+import re
 
 devname = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_557363239393515181E2-if00"
+
+pat = re.compile("(\d+) (on|off)")
 
 with serial.Serial(devname, 115200, timeout=1) as ser:
     print("opened serial port " + ser.name)
@@ -9,5 +13,8 @@ with serial.Serial(devname, 115200, timeout=1) as ser:
     while True:
         l = ser.readline()
         if l:
-            print(l)
+            m = re.search(pat)
+            print m.group(1)
+            print m.group(2)
+            
             
