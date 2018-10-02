0c435ef87b752fe42537d73635053bb23251d7ea
alex <alex@slab.org>
2018-10-02 16:13:09 +0100

    / ‘sense/trigger.py’

--- sense/trigger.py
+++ sense/trigger.py
@@ -4,7 +4,7 @@ devname = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_55736323939351518
 
 with serial.Serial('/dev/ttyS1', 19200, timeout=1) as ser:
     s = serial.Serial(devname)
-    print("opened serial port " + print(ser.name))
+    print("opened serial port " + ser.name)
 
     while true:
         l = ser.readline
