65714423751382fa79086467ff084bd722f1d451
alex <alex@slab.org>
2018-10-08 13:05:21 +0200

    / ‘sense/trigger.py’

--- sense/trigger.py
+++ sense/trigger.py
@@ -104,7 +104,7 @@ with serial.Serial(devname, 9600, timeout=1) as ser:
                     if maxval == None or maxval < filtered:
                         maxval = filtered
                     delta = maxval - minval
-                    perc = (filtered - minval) / delta
+                    #perc = (filtered - minval) / delta
                     co = perc * 2000.0
                     print("%s %d %f %f" % (m.group(1), int(m.group(2)), int(m.group(3)), int(m.group(4))))
                 dirt("foley",
